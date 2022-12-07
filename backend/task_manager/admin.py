7ac2ca2900955af0d96bd9baaeeb9e739182a564
eduardodangelo <eduardo@siteassist.co>
2022-12-07 20:48:50 +0000

    create backend models and register on admin site

--- backend/task_manager/admin.py
+++ backend/task_manager/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from .models import Project, Okr, Note, Status, Task, Checklist, Sprint
 
+class NoteInline(admin.TabularInline):
+    model = Note
+    extra = 0
+
+
+class SprintInline(admin.TabularInline):
+    model = Sprint
+    extra = 0
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    fieldsets = [
+        (None, {'fields': ['id', 'title', 'status', 'okr', 'sprint', 'project']}),
+    ]
+    extra = 0
+
+
+class OkrInline(admin.TabularInline):
+    model = Okr
+    fieldsets = [
+        (None, {'fields': ['id', 'title', 'status']}),
+    ]
+    extra = 0
+
+
+class ChecklistInline(admin.TabularInline):
+    model = Checklist
+    extra = 0
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'created_at', 'status']}),
+    ]
+    inlines = [OkrInline, SprintInline, NoteInline]
+
+
+class OkrAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'description', 'created_at', 'status', 'project']}),
+    ]
+    inlines = [TaskInline]
+
+
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'description', 'created_at', 'status', 'okr', 'sprint']}),
+    ]
+    inlines = [ChecklistInline]
+
+
+class SprintAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'created_at', 'project', 'result', 'completed']}),
+    ]
+    inlines = [TaskInline]
+
+admin.site.register(Okr, OkrAdmin)
+admin.site.register(Status)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Sprint, SprintAdmin)
 # Register your models here.
--- backend/task_manager/models.py
+++ backend/task_manager/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Status(models.Model):
+    title = models.CharField(max_length=120)
+    color_code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField('created at', default=timezone.now)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, default=None)
+
+    def __str__(self):
+        return self.title
+
+
+class Okr(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField('created at', default=timezone.now)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, default=None)
+
+    def __str__(self):
+        return self.title
+
+
+class Sprint(models.Model):
+    title = models.CharField(max_length=300)
+    created_at = models.DateTimeField('created at', default=timezone.now)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    result = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    created_at = models.DateTimeField('created at', default=timezone.now)
+    description = models.TextField(null=True, blank=True)
+    okr = models.ForeignKey(Okr, on_delete=models.CASCADE)
+    sprint = models.ForeignKey(Sprint, on_delete=models.PROTECT, null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Checklist(models.Model):
+    title = models.CharField(max_length=300)
+    created_at = models.DateTimeField('created at', default=timezone.now)
+    completed = models.BooleanField(default=False)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Note(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField('created at', default=timezone.now)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
